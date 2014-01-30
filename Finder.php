4df1d88570b2e6f86d2f0fa78f612d6a081016db
ajmorenodelarosa <ajmorenodelarosa@ubuntu.(none)>
2014-01-30 13:11:02 +0100

    first commit

---
+++ Finder.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace MongoSeach;
+
+class Finder {
+
+
+  private $collectionName;
+  private $db;
+  private $client;
+
+  public function __construct($collectionName, $dbName)
+  {
+   
+    $this->collectionName = $collectionName;
+    $m = new MongoClient(); // connect
+    $this->client = $m;
+    $this->db = $m->selectDB($dbName);
+
+  }
+
+
+  public function find($query) {
+
+   $result = $this->db->command(array("text" => $this->collectionName, 'search' => $query ));
+   print_r($result);
+   die();
+
+  }
+
+
+}
