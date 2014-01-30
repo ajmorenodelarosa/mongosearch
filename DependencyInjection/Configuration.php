ebfc6567f5c2b0684e33d970a94120ef039805dd
ajmorenodelarosa <ajmorenodelarosa@gmail.com>
2014-01-30 18:26:29 +0100

    Configuration class created

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MongoSearchBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+}
