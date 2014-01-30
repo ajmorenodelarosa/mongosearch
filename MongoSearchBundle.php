0b2c081c2bcede5e26b7ede4587655e279898e4d
ajmorenodelarosa <ajmorenodelarosa@ubuntu.(none)>
2014-01-30 16:49:53 +0100

    wrapping previous code into a bundle and a service

---
+++ MongoSearchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MongoSearch;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MongoSearchBundle extends Bundle
+{
+}
---
+++ Services/Finder/Finder.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace MongoSearch\Services\Finder;
+
+class Finder {
+
+
+  private $collectionName;
+  private $db;
+  private $client;
+
+  public function __construct($collectionName, $dbName)
+  {
+   
+    $this->collectionName = $collectionName;
+    $m = new \MongoClient(); // connect
+    $this->client = $m;
+    $this->db = $m->selectDB($dbName);
+
+  }
+
+
+  public function find($query) {
+
+   $result = $this->db->command(array("text" => $this->collectionName, 'search' => $query ));
+   return $result;
+
+  }
+
+
+}
